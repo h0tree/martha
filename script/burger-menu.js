8c0cb7b8fce982a9889693376c2c1aa171cd4e5f
h0tree <142096120+h0tree@users.noreply.github.com>
2026-03-23 23:16:58 +0300

    Решение бага

--- script/burger-menu.js
+++ script/burger-menu.js
@@ -2,14 +2,14 @@ let button = document.querySelector(".menu-button");
 let header = document.querySelector(".header");
 let array = document.querySelectorAll(".menu-button-arrow")
 let nav = document.querySelector(".header-listmenu")
-let title = document.querySelector(".header-title")
+let titleHeader = document.querySelector(".header-title")
 
-button.addEventListener("touchstart", () => {
+button.addEventListener("click", () => {
     header.classList.toggle("header-burger")
     button.classList.toggle("menu-button-active")
     array.forEach(element => {
         element.classList.toggle("menu-button-arrow-active")
     });
     nav.classList.toggle("header-listmenu-active");
-    title.classList.toggle("header-listmenu-active")
+    titleHeader.classList.toggle("header-listmenu-active")
 })
